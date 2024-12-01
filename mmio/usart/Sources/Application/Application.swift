594eaa954b3d7224e265099dfee40bef1f2af6d6
Jesse L. Zamora <xtremekforever@gmail.com>
2024-12-01 20:39:02 +0000

    Add initial working USART echo code
     - It's not perfect and print() is still not working, but it's something.

--- mmio/usart/Sources/Application/Application.swift
+++ mmio/usart/Sources/Application/Application.swift
@@ -10,12 +10,33 @@ struct Main {
 
         // Configure peripherals that are used
         board.configureLed()
+        board.configureUsart(baud: 115200)
+
+        //print("Hello Embedded Swift")
 
         while true {
+
+            // Wait for input byte
+            usart2.waitRxBufferFull()
+
+            // Read byte
+            let byte = usart2.rx()
+
             board.setLed(.on)
-            systick.delay(ticks: 100)
+
+            // Echo back
+            usart2.tx(value: byte)
+            usart2.waitTxBufferEmpty()
+
+            // Send a "\n" if the byte is a "\r"
+            if byte == UInt8(ascii: "\r") {
+                usart2.tx(value: UInt8(ascii: "\n"))
+                usart2.waitTxBufferEmpty()
+            }
+
+            systick.delay(ticks: 5)
+
             board.setLed(.off)
-            systick.delay(ticks: 300)
         }
     }
 }
